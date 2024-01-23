9f49c5e1c27819c961b51b95f151e966ebd28772
tunameatball <kh9768@gmail.com>
2024-01-23 09:35:01 +0900

    go, channel

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 var out string = "out"
@@ -10,19 +11,32 @@ var out string = "out"
 func main() {
 	// fmt.Println("Hello World")
 	// var name string = "kkh"
-	name := "kkh"
-	name = "lynn"
-	fmt.Println(name)
+	// name := "kkh"
+	// name = "lynn"
+	// fmt.Println(name)
 
-	fmt.Println(multiply(2, 2))
+	// fmt.Println(multiply(2, 2))
 
-	totalLength, _ := lenAndUpper(name)
-	fmt.Println(totalLength)
-	repeatMe("1", "2", "3", "4")
+	// totalLength, _ := lenAndUpper(name)
+	// fmt.Println(totalLength)
+	// repeatMe("1", "2", "3", "4")
 
-	total := superAdd(1, 2, 3, 4, 5, 6, 7)
+	// total := superAdd(1, 2, 3, 4, 5, 6, 7)
 
-	fmt.Println(total)
+	// fmt.Println(total)
+
+	// go myCount("KKH1")
+	// myCount("KKH2")
+
+	c := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isPerson(person, c)
+		// result := <-c
+		if <-c {
+			fmt.Println(person)
+		}
+	}
 }
 
 func multiply(a int, b int) int {
@@ -30,6 +44,7 @@ func multiply(a int, b int) int {
 }
 
 func lenAndUpper(name string) (int, string) {
+	defer myCount("KKH3")
 	defer fmt.Println("I'm done")
 	fmt.Println("Run")
 	return len(name), strings.ToUpper(name)
@@ -45,4 +60,16 @@ func superAdd(numbers ...int) int {
 		total += number
 	}
 	return total
+}
+
+func myCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "count", i)
+		time.Sleep(time.Second)
+	}
+}
+
+func isPerson(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	c <- true
 }
